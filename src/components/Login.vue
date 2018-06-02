42e0e5babc3848f56a769ec288c9e618d7c0d290
Artyom O <18art12@gmail.com>
2018-06-02 19:22:09 +0300

    sign-in page created

---
+++ src/components/Login.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="login">
+    <h3>Sign In</h3>
+    <input type="text" placeholder="Email">
+    <input type="password" placeholder="Password">
+    <button>Go</button>
+    <p>or you can <a href="#">Create account</a></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data: function() {
+    return {}
+  },
+  methods: {}
+}
+</script>
+
+<style lang="css" scoped>
+
+</style>
